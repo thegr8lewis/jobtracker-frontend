afaa39a0648b3c2b8d9850e6db1d548e29a38774
thegr8lewis <lewis.nyakaru@gmail.com>
2025-08-21 14:58:25 +0300

    wefwgr

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -130,7 +130,7 @@
 
 
 import axios from 'axios';
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000/api';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || ' https://job-tracker-backend-ztii.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
